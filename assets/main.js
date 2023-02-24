44c97ffcfefa45f1ec515bd6ddfb745fc0cdfde5
Lautaro Gutierrez <lauchagutierrez117@gmail.com>
2023-02-24 10:00:55 -0300

    modificacion en estilos de la card

--- assets/main.js
+++ assets/main.js
@@ -7,7 +7,7 @@
         <div class="card-body">
             <h5 class="card-title">${event.name}</h5>
              <p class="card-text">${event.description}</p>
-             <a href="./details.html" class="btn btn-primary">Go somewhere</a><span class="ms-3 text-muted">Price: $${event.price}<span>
+             <a href="./details.html" class="btn btn-primary">Go somewhere</a><span class="ms-3">Price: $${event.price}<span>
         </div>
          </div>`; 
 
